4fd058173da6628f2c8b0e9ca57c36491de0b3d7
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-01 11:04:14 +0100

    Done

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,42 @@
   // check if the current is even or odd
 
   // report the result to the screen
+
+
+  function evenOrNot(){
+
+    for(i = 0; i <= 20; i++){
+      if (i % 2 == 0){
+        console.log(`${i} is even`)
+      }
+    }
+  }
+
+
+
+  function evenOrNot1(){
+    var i = 0;
+    while(i <= 20){
+        if (i % 2 == 0){
+          console.log(`${i} is even`)
+                       }
+          i++;
+                  }
+           
+        }
+
+        function evenOrNot2(){
+          var i = 0;
+                  do {
+              if (i % 2 == 0){
+                console.log(`${i} is even`)
+                             }
+                    i++;
+                    } while (i <= 20)
+                 
+              }
+
+evenOrNot();
+evenOrNot1();
+evenOrNot2();
+        
