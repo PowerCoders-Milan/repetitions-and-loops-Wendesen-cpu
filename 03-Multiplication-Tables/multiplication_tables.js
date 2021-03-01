453a83e4c823e8ade66e96828d1c14ba2f028660
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-01 11:26:26 +0100

    Done without the optional one

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,10 @@
 // write your code below
+
+function multiply_By9(){
+    var array = [0,1,2,3,4,5,6,7,8,9,10];
+    array.forEach(function(item){
+        console.log(9*item);
+    })
+}
+
+multiply_By9();
